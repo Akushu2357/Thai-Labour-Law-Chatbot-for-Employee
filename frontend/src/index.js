1098e609c4a2eca204b09765233b5d0b90010ded
Akushu <tang.tang2004@gmail.com>
2025-11-05 23:00:31 +0700

    Add responsive navbar and update styles for improved UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NavBar from './navbar';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+const navbar = ReactDOM.createRoot(document.getElementById('navbar'));
+navbar.render(
+  <React.StrictMode>
+    <NavBar />
   </React.StrictMode>
 );
 
